b91e3933db346e7bd0b24fec493ab85efaa01cd4
Sergey <raven@Sergeys-MacBook-Air.local>
2017-09-20 23:01:00 +0300

    Added Game field generator and Gestures recognizers

--- ABCGames_Test/GameField.swift
+++ ABCGames_Test/GameField.swift
@@ -8,6 +8,126 @@
 
 import Foundation
 import UIKit
+import AudioToolbox
 
 @IBDesignable class GameField : UIView{
+    var xDimension : Int = 11
+    var yDimension : Int = 10
+    private var subViewsHashTable = [String : UIView]()
+    private var xOffset : CGFloat!
+    private var yOffset : CGFloat!
+    private var size : CGFloat!
+    private var numberOfCapturedViews : Int = 0{
+        didSet{
+            if (numberOfCapturedViews % 3 == 0) && (numberOfCapturedViews != 0){
+                bonusPoints += 1
+            }
+            
+        }
+    }
+    private var bonusPoints : Int = 0{
+        didSet{
+            //Notify MainViewController
+        }
+    }
+    private var penaltyPoints : Int = 0{
+        didSet{
+            //Notify MainViewController
+        }
+    }
+    private var previousSubviewForPanGesture : UIView?
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        self.backgroundColor = .clear
+        self.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(handleTapGesture(gesture:))))
+        self.addGestureRecognizer(UIPanGestureRecognizer(target: self, action: #selector(handlePanGesture(gesture:))))
+    }
+    
+    override func draw(_ rect: CGRect) {
+        super.draw(rect)
+        //Clear existing subviews and values
+        resetGameField()
+        
+        //Create new ones
+        createSubview()
+    }
+    
+    @objc private func handleTapGesture(gesture: UITapGestureRecognizer){
+        let location = gesture.location(in: self)
+        //Get row and column of current tap point
+        let row = Int((location.x - xOffset) / size)
+        let column = Int((location.y - yOffset) / size)
+        let selectedSubview = subViewsHashTable["\(row)|\(column)"]
+        if selectedSubview?.backgroundColor == .black{
+            AudioServicesPlayAlertSound(SystemSoundID(kSystemSoundID_Vibrate))
+            penaltyPoints += 1
+        } else {
+            selectedSubview?.backgroundColor = .black
+        }
+    }
+    
+    @objc private func handlePanGesture(gesture: UIPanGestureRecognizer){
+        let location = gesture.location(in: self)
+        //Get row and column of current tap point
+        let row = Int((location.x - xOffset) / size)
+        let column = Int((location.y - yOffset) / size)
+        let selectedSubview = subViewsHashTable["\(row)|\(column)"]
+        if selectedSubview != previousSubviewForPanGesture{
+            previousSubviewForPanGesture = selectedSubview
+            if selectedSubview?.backgroundColor == .black{
+                AudioServicesPlayAlertSound(SystemSoundID(kSystemSoundID_Vibrate))
+                penaltyPoints += 1
+            } else {
+                selectedSubview?.backgroundColor = .black
+                numberOfCapturedViews += 1
+            }
+        }
+        
+        
+        if gesture.state == .cancelled || gesture.state == .ended || gesture.state == .failed{
+            numberOfCapturedViews = 0
+        }
+        
+        
+    }
+    
+    private func createSubview(){
+        if self.frame.width / CGFloat(xDimension) >= self.frame.height / CGFloat(yDimension){
+            size = self.frame.height / CGFloat(yDimension)
+            xOffset = (self.frame.width - size * CGFloat(xDimension)) / 2
+            yOffset = 0
+        } else {
+            size = self.frame.width / CGFloat(xDimension)
+            xOffset = 0
+            yOffset = (self.frame.height - size * CGFloat(yDimension)) / 2
+        }
+        
+        for row in 0..<xDimension{
+            for column in 0..<yDimension{
+                let origin = CGPoint(x: CGFloat(row) * size + xOffset, y: CGFloat(column) * size + yOffset)
+                let subview = UIView(frame: CGRect(origin: origin, size: CGSize(width: size, height: size)))
+                
+                subview.backgroundColor = .red
+                subview.layer.borderWidth = 1
+                subview.layer.borderColor = UIColor.white.cgColor
+                self.addSubview(subview)
+                
+                //Add subview in table for key : row | column
+                subViewsHashTable["\(row)|\(column)"] = subview
+                
+            }
+        }
+    }
+    
+    private func resetGameField(){
+        for (_, value) in subViewsHashTable{
+            value.removeFromSuperview()
+        }
+        subViewsHashTable.removeAll()
+        numberOfCapturedViews = 0
+        bonusPoints = 0
+        penaltyPoints = 0
+    }
 }
--- MainViewController.swift
+++ MainViewController.swift
@@ -16,6 +16,11 @@ class MainViewController: UIViewController {
     @IBOutlet weak var bonusPointsLabel: UILabel!
     @IBOutlet weak var penaltyPointsLabel: UILabel!
     
+    //Redraw GameField when device rotated
+    override func viewWillTransition(to size: CGSize, with coordinator: UIViewControllerTransitionCoordinator) {
+        super.viewWillTransition(to: size, with: coordinator)
+        gameFieldView.setNeedsDisplay()
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
